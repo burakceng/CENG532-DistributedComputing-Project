630f3229b838ef8d66e4abc429493c1bffda26ee
burakceng <burak-hocaoglu@hotmail.com>
2018-12-29 21:09:42 +0300

    Mininet script is updated to fork nodes when experimenting.

--- script.py
+++ script.py
@@ -66,7 +66,7 @@ if __name__ == '__main__':
 		if pid == 0:
 			start_time = time.time()
 			h_i = net.get(node)
-			h1.cmd('sudo python Node.py h1 ' + str(packets_per_node) + ' ' + ' '.join(map(str, sequence_dict['h1'])) + ' ' + str(period) + ' ' + getFileName(topo_type) + ' ' + poisson + ' LOG_h1.txt')
+			h_i.cmd('sudo python Node.py ' + h_i + ' ' + str(packets_per_node) + ' ' + ' '.join(map(str, sequence_dict['h1'])) + ' ' + str(period) + ' ' + getFileName(topo_type) + ' ' + poisson + ' LOG_h1.txt')
 			while time.time() - start_time:
 				time.sleep(1)
 		else:
