9a08b6ee0520d04ef40c43f2629e2d28684c6528
Rasit Ozdemir <rasitozdemir@windowslive.com>
2018-12-29 18:38:14 +0300

    mininet script is implemented

--- script.py
+++ script.py
@@ -6,12 +6,10 @@ from mininet.util import dumpNodeConnections
 from mininet.log import setLogLevel
 from mininet.node import CPULimitedHost
 from mininet.link import TCLink
-
+import numpy as np
 
 class Topology(Topo):
-
-	# The topology consists of 1 switch in the middle, 1 controller and 4 hosts in a star topology.
-
+	# The topology consists of 1 switch in the middle, 1 controller and 5 hosts in a star topology.
 	def build(self, n=2):
 		# Adding the switch in the middle of the network
 		switch = self.addSwitch('s1')
@@ -24,23 +22,50 @@ class Topology(Topo):
 			# 10% of packet loss rate and 1000 packets as queuing limit. 
 			self.addLink(host, switch, bw=4, delay='10ms', loss=10, max_queue_size=1000, use_htb=True)
 
-def perfTest():
+def initTopo():
 	# Regression testing
 	topo = Topology(n=5)
 	net = Mininet(topo, host = CPULimitedHost, link = TCLink)
 	net.start()
-	#print "Displaying connections on net:"
-	#dumpNodeConnections(net.hosts)
-	#print "Testing bandwith between h1 and h4:"
-	#h1, h4 = net.get('h1', 'h4')
-	#net.iperf((h1, h4))
-	#net.stop()
+
+def getFileName(topo_type):
+	if topo_type == 2:
+		return 'configuration2.xml'
+	elif topo_type == 3:
+		return 'configuration3.xml'
+	elif topo_type == 4:
+		return 'configuration4.xml'
 
 if __name__ == '__main__':
 	setLogLevel('info')
-#topos = { 'mytopo': ( lambda: Topology(n=5) ) }
-	perfTest()
 
-	i = 0
-	while True:
-		i += 1
+	packets_per_node = int(sys.argv[1])
+	topo_type = int(sys.argv[2])
+	period = float(sys.argv[3])
+	poisson = sys.argv[4]
+	
+	sequence_list = range(1, 5 * packets_per_node + 1)
+	
+	sequence_dict = {}
+	
+	for i in xrange(5):
+		packet_numbers = sorted(np.random.choice(sequence_list, size=packets_per_node, replace=False))
+		node_name = 'h%d' % (i + 1)
+		sequence_dict[node_name] = packet_numbers
+		for j in packet_numbers:
+			sequence_list.remove(j)
+	
+	initTopo()
+
+	h1.cmd('sudo python h1 ' + str(packets_per_node) + ' ' + ' '.join(map(str, sequence_dict['h1'])) + ' ' + str(period) + ' ' + getFileName(topo_type) + ' ' + poisson + ' LOG_h1.txt')
+	h2.cmd('sudo python h2 ' + str(packets_per_node) + ' ' + ' '.join(map(str, sequence_dict['h2'])) + ' ' + str(period) + ' ' + getFileName(topo_type) + ' ' + poisson + ' LOG_h2.txt')
+	h3.cmd('sudo python h3 ' + str(packets_per_node) + ' ' + ' '.join(map(str, sequence_dict['h3'])) + ' ' + str(period) + ' ' + getFileName(topo_type) + ' ' + poisson + ' LOG_h3.txt')
+	h4.cmd('sudo python h4 ' + str(packets_per_node) + ' ' + ' '.join(map(str, sequence_dict['h4'])) + ' ' + str(period) + ' ' + getFileName(topo_type) + ' ' + poisson + ' LOG_h4.txt')
+	h5.cmd('sudo python h5 ' + str(packets_per_node) + ' ' + ' '.join(map(str, sequence_dict['h5'])) + ' ' + str(period) + ' ' + getFileName(topo_type) + ' ' + poisson + ' LOG_h5.txt')
+	
+	
+	
+	
+	
+	
+	
