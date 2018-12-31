076bf632bd054332575853becef1faad7a6190c1
Rasit Ozdemir <rasitozdemir@windowslive.com>
2018-12-31 17:22:39 +0300

    exp lambda

--- lambda_exp.sh
+++ lambda_exp.sh
@@ -1,2 +1,32 @@
-sudo python script.py 2 2 1 5
+sudo python script.py 3 2 1 5
+sudo python script.py 3 2 1 5
+sudo python script.py 3 2 1 5
+sudo python script.py 3 2 1 5
+sudo python script.py 3 2 1 5
+sudo python script.py 3 2 1 5
+sudo python script.py 3 2 1 5
+sudo python script.py 3 2 1 5
+sudo python script.py 3 2 1 5
+
+sudo python script.py 3 2 1 3
+sudo python script.py 3 2 1 3
+sudo python script.py 3 2 1 3
+sudo python script.py 3 2 1 3
+sudo python script.py 3 2 1 3
+sudo python script.py 3 2 1 3
+sudo python script.py 3 2 1 3
+sudo python script.py 3 2 1 3
+sudo python script.py 3 2 1 3
+sudo python script.py 3 2 1 3
+
+sudo python script.py 3 2 1 2
+sudo python script.py 3 2 1 2
+sudo python script.py 3 2 1 2
+sudo python script.py 3 2 1 2
+sudo python script.py 3 2 1 2
+sudo python script.py 3 2 1 2
+sudo python script.py 3 2 1 2
+sudo python script.py 3 2 1 2
+sudo python script.py 3 2 1 2
+sudo python script.py 3 2 1 2
 
